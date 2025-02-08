ae813ba4e3ca177ae86d2e096fff79335601f672
K-Road <game@chrodeprime.com>
2025-02-08 23:30:57 +1100

    added att & hp

--- Scripts/deck.gd
+++ Scripts/deck.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 @export var CARD_DRAW_SPEED = 0.5
-var player_deck = ["knight", "knight", "knight"]
-
+var player_deck = ["Knight", "Archer", "Demon"]
+var card_database_reference
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	player_deck.shuffle()
 	$RichTextLabel.text = str(player_deck.size())
+	card_database_reference = preload("res://Scripts/CardDatabase.gd")
 
 func draw_card():
 	var card_drawn = player_deck[0]
@@ -20,9 +22,12 @@ func draw_card():
 	$RichTextLabel.text = str(player_deck.size())
 	print("DRAW CARD")
 	var card_scene = preload("res://Scenes/card.tscn")
-
 	var new_card = card_scene.instantiate()
+	var card_image_path = str("res://Assets/Cards/" + card_drawn + "_Card.png")
+	new_card.get_node("CardImage").texture = load(card_image_path)
+	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn][0])
+	new_card.get_node("Health").text = str(card_database_reference.CARDS[card_drawn][1])
 	$"../CardManager".add_child(new_card)
 	new_card.name = "Card"
 	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
-	
+	new_card.get_node("AnimationPlayer").play("Card_Flip")
